7094ef363ea9f1344ae1e2771f6a9034e37f9a7c
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-11 00:37:07 -0400

    setup index and add json

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import photosRouter from './routes/photo.js';
+import tagsRouter from './routes/tag.js';
+import cors from cors;
 const app = express();
 
 const PORT = process.env.PORT || 8081;
+
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(express.json());
+
+app.use("/tag",tagsRouter);
+app.use("/photo",photosRouter);
+
 app.listen(8080, function () {
-  console.log('listening to port: 8080', PORT);
+  console.log(`Server is listening on PORT ${PORT}`);
 });
