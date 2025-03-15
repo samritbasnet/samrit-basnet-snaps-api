d23f4bc5b887f5fc8268849e0a0ffa3d387e3faf
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-15 19:59:18 -0400

    api routes changes to make connection succesfull

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import photosRouter from './routes/photo.js';
-import tagsRouter from './routes/tag.js';
-import cors from cors;
+import tagsRouter from './routes/tags.js';
+import cors from 'cors';
 const app = express();
 
 const PORT = process.env.PORT || 8081;
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 8081;
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-  res.send("Succesfull connection");
-})
-app.use("/tag",tagsRouter);
-app.use("/photo",photosRouter);
+app.get('/', (req, res) => {
+  res.send('Succesfull connection');
+});
+app.use('/tags', tagsRouter);
+app.use('/photos', photosRouter);
 
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.log(`Server is listening on PORT ${PORT}`);
 });
